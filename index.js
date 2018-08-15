04cd5f9b818d4213ab9ce1b5ade2848ff8290ed4
Carson Rey <c.rey494@gmail.com>
2018-08-15 22:27:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ function destructivelyAppendKitten(name) {
   return kittens.push(name);
 }
 
-function destructivelyAppendKitten
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name);
+}
